bd76e2cfc2e6a4257d5f971d09afe3c00b8429b9
rahil Patel <rahil.patel.hs@gmail.com>
2021-11-25 00:04:51 -0500

    updated db connection

--- config/db.js
+++ config/db.js
@@ -5,6 +5,6 @@
  * Date: 12-11-2021
  */
 module.exports = {
-    "URI": "mongodb+srv://sohil:sohil@clusterlearning.j3pl2.mongodb.net/tunos?retryWrites=true&w=majority",
+    "URI": "mongodb+srv://Tunos:c8xnV6S5f4wyzgf@cluster0.3q5tq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
     "Secret": '9226641e4013e8281d1069dddeec9a29a26ec7a352414aac502cb0e22138e853bee7ce7764bc2f2c7da4d44fb21b1b5c'
 }
