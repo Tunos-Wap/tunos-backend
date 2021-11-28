cad539d28b50e87ed7040332b82c52393748f60f
rahil Patel <rahil.patel.hs@gmail.com>
2021-11-28 16:21:12 -0500

    Added Authroiztion in users API

--- routes/users.js
+++ routes/users.js
@@ -4,24 +4,33 @@
  * Student Name: Rahilkumar Patel
  * Date: 19-11-2021
  */
- var express = require('express');
- var router = express.Router();
- 
- const usersController = require('../controllers/usersController');
- 
- /* GET Route for the Users List - READ Operation */
- router.get('/', usersController.index);
- 
- /* GET Route for the Users Details - READ Operation */
- router.get('/:id', usersController.details);
- 
- /* POST Route for the new Users create - WRITE Operation */
- router.post('/', usersController.create);
- 
- /* PUT Route for the existing Users update - WRITE Operation */
- router.put('/:id', usersController.update);
- 
- /* DELETE Route for the existing Users delete - WRITE Operation */
- router.delete('/:id', usersController.delete);
+var express = require('express');
+var router = express.Router();
 
- module.exports = router;
+// helper function for guard purposes
+function requireAuth(req, res, next) {
+    // check if the user is logged in
+    if (!req.isAuthenticated()) {
+        return res.redirect('/login');
+    }
+    next();
+}
+
+const usersController = require('../controllers/usersController');
+
+/* GET Route for the Users List - READ Operation */
+router.get('/', requireAuth, usersController.index);
+
+/* GET Route for the Users Details - READ Operation */
+router.get('/:id', requireAuth, usersController.details);
+
+/* POST Route for the new Users create - WRITE Operation */
+router.post('/', requireAuth, usersController.create);
+
+/* PUT Route for the existing Users update - WRITE Operation */
+router.put('/:id', requireAuth, usersController.update);
+
+/* DELETE Route for the existing Users delete - WRITE Operation */
+router.delete('/:id', requireAuth, usersController.delete);
+
+module.exports = router;
